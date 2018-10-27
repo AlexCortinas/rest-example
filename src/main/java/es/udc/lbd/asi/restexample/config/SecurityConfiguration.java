4b57184e50f482ce237216cd3549ef9543689d83
Álex Cortiñas <mail@alexcortinas.es>
2018-10-27 12:10:37 +0200

    Configurada la seguridad de la aplicación con jwt

--- src/main/java/es/udc/lbd/asi/restexample/config/SecurityConfiguration.java
+++ src/main/java/es/udc/lbd/asi/restexample/config/SecurityConfiguration.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +18,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import es.udc.lbd.asi.restexample.security.JWTConfigurer;
+import es.udc.lbd.asi.restexample.security.MyAccessDeniedHandler;
+import es.udc.lbd.asi.restexample.security.MyUnauthorizedEntryPoint;
 import es.udc.lbd.asi.restexample.security.MyUserDetailsService;
+import es.udc.lbd.asi.restexample.security.TokenProvider;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -27,6 +34,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
+    @Autowired
+    private MyUnauthorizedEntryPoint myUnauthorizedEntryPoint;
+
+    @Autowired
+    private MyAccessDeniedHandler myAccessDeniedHandler;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+    
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -39,15 +55,33 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
         http
-            .csrf().disable()
+            .csrf()
+            .disable()
+            .exceptionHandling()
+            .authenticationEntryPoint(myUnauthorizedEntryPoint)
+            .accessDeniedHandler(myAccessDeniedHandler)
+        .and()
             .headers().frameOptions().disable()
         .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
-            .antMatchers("/**").permitAll();
+            .antMatchers("/api/authenticate").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/register").permitAll()
+            .antMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+            .antMatchers("/**").authenticated()
+        .and()
+            .apply(securityConfigurerAdapter());;
+        // @formatter:on
     }
 
     @Bean
@@ -67,5 +101,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         } catch (Exception e) {
             throw new BeanInitializationException("SecurityConfiguration.configureAuth failed", e);
         }
+    }
+
+    private JWTConfigurer securityConfigurerAdapter() {
+        return new JWTConfigurer(tokenProvider);
     }
 }
