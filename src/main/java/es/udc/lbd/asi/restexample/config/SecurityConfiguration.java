b2e3f39a78fa099c93767e1e0cfd7fd922de1f2f
Álex Cortiñas <mail@alexcortinas.es>
2018-10-27 12:04:55 +0200

    Configurar authentication manager

--- src/main/java/es/udc/lbd/asi/restexample/config/SecurityConfiguration.java
+++ src/main/java/es/udc/lbd/asi/restexample/config/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package es.udc.lbd.asi.restexample.config;
 
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,12 +16,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import es.udc.lbd.asi.restexample.security.MyUserDetailsService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Properties properties;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -44,5 +58,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 registry.addMapping("/**").allowedMethods("*").allowedOrigins(properties.getClientHost());
             }
         };
+    }
+
+    @Autowired
+    public void configureAuth(AuthenticationManagerBuilder auth) {
+        try {
+            auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
+        } catch (Exception e) {
+            throw new BeanInitializationException("SecurityConfiguration.configureAuth failed", e);
+        }
     }
 }
