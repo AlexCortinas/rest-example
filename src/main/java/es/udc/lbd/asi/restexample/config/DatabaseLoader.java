0117099433b9e35d758865a6ee81c60dcc124f8e
Álex Cortiñas <mail@alexcortinas.es>
2018-10-29 21:25:39 +0100

    Creando datos con Post asociados a Tags

--- src/main/java/es/udc/lbd/asi/restexample/config/DatabaseLoader.java
+++ src/main/java/es/udc/lbd/asi/restexample/config/DatabaseLoader.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.udc.lbd.asi.restexample.model.domain.Post;
+import es.udc.lbd.asi.restexample.model.domain.Tag;
 import es.udc.lbd.asi.restexample.model.exception.UserLoginExistsException;
 import es.udc.lbd.asi.restexample.model.repository.PostDAO;
+import es.udc.lbd.asi.restexample.model.repository.TagDAO;
 import es.udc.lbd.asi.restexample.model.repository.UserDAO;
 import es.udc.lbd.asi.restexample.model.service.UserService;
 
@@ -26,6 +28,9 @@ public class DatabaseLoader {
 
     @Autowired
     private PostDAO postDAO;
+
+    @Autowired
+    private TagDAO tagDAO;
 
     @Autowired
     private DatabaseLoader databaseLoader;
@@ -52,11 +57,29 @@ public class DatabaseLoader {
         userService.registerUser("laura", "laura");
         userService.registerUser("pedro", "pedro");
 
-        postDAO.save(new Post("Título 1", "Texto del primer post", userDAO.findByLogin("pepe")));
-        postDAO.save(new Post("Título 2", "Texto del segundo post", userDAO.findByLogin("maria")));
-        postDAO.save(new Post("Título 3", "Texto del tercero post", userDAO.findByLogin("maria")));
-        postDAO.save(new Post("Título 4", "Texto del cuarto post", userDAO.findByLogin("maria")));
-        postDAO.save(new Post("Título 5", "Texto del quinto post", userDAO.findByLogin("pepe")));
-        postDAO.save(new Post("Título 6", "Texto del sexto post", userDAO.findByLogin("pepe")));
+        Tag news = new Tag("news");
+        Tag podcast = new Tag("podcast");
+        Tag tech = new Tag("tech");
+
+        tagDAO.save(news);
+        tagDAO.save(podcast);
+        tagDAO.save(tech);
+
+        Post post = new Post("Título 1", "Texto del primer post", userDAO.findByLogin("pepe"));
+        post.getTags().add(news);
+        post.getTags().add(podcast);
+        postDAO.save(post);
+        post = new Post("Título 2", "Texto del segundo post", userDAO.findByLogin("maria"));
+        post.getTags().add(news);
+        post.getTags().add(tech);
+        postDAO.save(post);
+        post = new Post("Título 3", "Texto del tercero post", userDAO.findByLogin("maria"));
+        postDAO.save(post);
+        post = new Post("Título 4", "Texto del cuarto post", userDAO.findByLogin("maria"));
+        postDAO.save(post);
+        post = new Post("Título 5", "Texto del quinto post", userDAO.findByLogin("pepe"));
+        postDAO.save(post);
+        post = new Post("Título 6", "Texto del sexto post", userDAO.findByLogin("pepe"));
+        postDAO.save(post);
     }
 }
